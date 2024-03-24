0811272f03f0cc37b47497c369c76749bbaef3f2
Ali Saleh <ali_saleh-4@hotmail.fr>
2024-03-24 11:41:52 +0100

    adding middleware

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,23 +1,14 @@
 
 "use client";
 import {  useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 const Page =  () => {
-    const { data: session,status } = useSession();
-    const router = useRouter();
-    useEffect(() => {
-        if (status !== "authenticated" && status !=="loading") {
-          router.push("/");
-        }
-      }, [status, router]);
-
-
+    const { data: session } = useSession();
   return (
     <div >
       <h1>Mon nom : {session?.user?.name }</h1>
       <h1>Mon email : {session?.user?.email}</h1>
+   
     </div>
   );
 };
